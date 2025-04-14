9457ca57f726d919e8464639d724d2500b30cdc7
XZDang13 <xdang13@outlook.com>
2025-04-14 13:52:44 +0200

    add aligment

---
+++ ddpg_train.sh
@@ -0,0 +1,29 @@
+python ddpg_trainer.py --task=buttonpress --seed=100
+python ddpg_trainer.py --task=buttonpress --seed=200
+python ddpg_trainer.py --task=buttonpress --seed=300
+python ddpg_trainer.py --task=buttonpress --seed=400
+python ddpg_trainer.py --task=buttonpress --seed=500
+
+python ddpg_trainer.py --task=draweropen --seed=100
+python ddpg_trainer.py --task=draweropen --seed=200
+python ddpg_trainer.py --task=draweropen --seed=300
+python ddpg_trainer.py --task=draweropen --seed=400
+python ddpg_trainer.py --task=draweropen --seed=500
+
+python ddpg_trainer.py --task=push --seed=100
+python ddpg_trainer.py --task=push --seed=200
+python ddpg_trainer.py --task=push --seed=300
+python ddpg_trainer.py --task=push --seed=400
+python ddpg_trainer.py --task=push --seed=500
+
+python ddpg_trainer.py --task=pushback --seed=100
+python ddpg_trainer.py --task=pushback --seed=200
+python ddpg_trainer.py --task=pushback --seed=300
+python ddpg_trainer.py --task=pushback --seed=400
+python ddpg_trainer.py --task=pushback --seed=500
+
+python ddpg_trainer.py --task=windowopen --seed=100
+python ddpg_trainer.py --task=windowopen --seed=200
+python ddpg_trainer.py --task=windowopen --seed=300
+python ddpg_trainer.py --task=windowopen --seed=400
+python ddpg_trainer.py --task=windowopen --seed=500
