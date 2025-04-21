6483e33d49225846c63b6a89755f3b15155959ad
XZDang13 <xdang13@outlook.com>
2025-04-21 18:11:55 +0200

    update ppo_visual sh

--- ppo_visual_train.sh
+++ ppo_visual_train.sh
@@ -1,19 +1,19 @@
-python ppo_visual_trainer.py --task=draweropen --seed=200
-python ppo_visual_trainer.py --task=draweropen --seed=400
-python ppo_visual_trainer.py --task=draweropen --seed=600
+python ppo_trainer_visual.py --task=draweropen --seed=200
+python ppo_trainer_visual.py --task=draweropen --seed=400
+python ppo_trainer_visual.py --task=draweropen --seed=600
 
-python ppo_visual_trainer.py --task=hammer --seed=200
-python ppo_visual_trainer.py --task=hammer --seed=400
-python ppo_visual_trainer.py --task=hammer --seed=600
+python ppo_trainer_visual.py --task=hammer --seed=200
+python ppo_trainer_visual.py --task=hammer --seed=400
+python ppo_trainer_visual.py --task=hammer --seed=600
 
-python ppo_visual_trainer.py --task=pickplace --seed=200
-python ppo_visual_trainer.py --task=pickplace --seed=400
-python ppo_visual_trainer.py --task=pickplace --seed=600
+python ppo_trainer_visual.py --task=pickplace --seed=200
+python ppo_trainer_visual.py --task=pickplace --seed=400
+python ppo_trainer_visual.py --task=pickplace --seed=600
 
-python ppo_visual_trainer.py --task=pushback --seed=200
-python ppo_visual_trainer.py --task=pushback --seed=400
-python ppo_visual_trainer.py --task=pushback --seed=600
+python ppo_trainer_visual.py --task=pushback --seed=200
+python ppo_trainer_visual.py --task=pushback --seed=400
+python ppo_trainer_visual.py --task=pushback --seed=600
 
-python ppo_visual_trainer.py --task=windowopen --seed=200
-python ppo_visual_trainer.py --task=windowopen --seed=400
-python ppo_visual_trainer.py --task=windowopen --seed=600
+python ppo_trainer_visual.py --task=windowopen --seed=200
+python ppo_trainer_visual.py --task=windowopen --seed=400
+python ppo_trainer_visual.py --task=windowopen --seed=600
